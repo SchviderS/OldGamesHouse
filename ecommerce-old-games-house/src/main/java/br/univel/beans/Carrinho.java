efe5320778a0d6b4d7ca68efc8ba900f5f39194e
Alexandre Schviderski <erdnaxelaschvi@gmail.com>
2015-11-12 22:10:06 -0200

    Criados os EJB para o carrinho e para controlar o Index, para listar os produtos cadastrados

---
+++ ecommerce-old-games-house/src/main/java/br/univel/beans/Carrinho.java
@@ -0,0 +1,33 @@
+package br.univel.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import br.univel.model.Produto;
+import java.io.Serializable;
+
+@SessionScoped
+@Named
+public class Carrinho implements Serializable
+{
+	
+	private static final long serialVersionUID = 1L;
+	
+	List<Produto> produtos = new ArrayList<Produto>();
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
+	public void addProduto(Produto p){
+		produtos.add(p);
+	}
+	
+}
---
+++ ecommerce-old-games-house/src/main/java/br/univel/service/IndexController.java
@@ -0,0 +1,36 @@
+package br.univel.service;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+import br.univel.model.Produto;
+import br.univel.rest.ProdutoEndpoint;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Stateless
+@LocalBean
+public class IndexController implements Serializable
+{
+
+   private static final long serialVersionUID = -1L;
+   
+   private List<Produto> produtos = new ProdutoEndpoint().listAll(null, null);
+
+   public List<Produto> getProdutos() {
+	   return produtos;
+   }
+   
+   public Produto getProduto(int id){
+	   Produto prod = null;
+	   for(Produto p: produtos){
+		   if(p.getId() == id)
+			   prod = p;
+	   }
+	   return prod;
+   }
+   
+   
+}
